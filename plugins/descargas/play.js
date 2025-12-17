7ebbcaa58924370d0d48dbc29a482d1f197ceb05
AngelBotsD <angelperez22448@gmail.com>
2025-12-16 20:59:56 -0600

    Actualizar play.js

--- plugins/descargas/play.js
+++ plugins/descargas/play.js
@@ -13,7 +13,7 @@ const TMP_DIR = path.join(process.cwd(), "tmp")
 if (!fs.existsSync(TMP_DIR)) fs.mkdirSync(TMP_DIR, { recursive: true })
 
 const CACHE_FILE = path.join(TMP_DIR, "cache.json")
-const SKY_BASE = (process.env.API_BASE || "https://api-sky.ultraplus.click").replace(/\/+$/, "")
+const SKY_BASE = (process.env.API_BASE || "https://api-sky.ultraplus.click/").replace(/\/+$/, "")
 const SKY_KEY = process.env.API_KEY || "Angxllll"
 
 const MAX_CONCURRENT = 3
